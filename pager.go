13ee49e32b3feb7c9d44002054db13dec8cf3816
Hinagiku Soranoba <soranoba@gmail.com>
2022-02-28 14:54:58 +0900

    Fix: the possibility of panic instead of error when the DB connection is broken. (#18)

--- pager.go
+++ pager.go
@@ -90,9 +90,7 @@ func pagerHandleBeforeQuery(db *gorm.DB) {
 	preloads := tx.Statement.Preloads
 	tx.Statement.Preloads = map[string][]interface{}{}
 
-	if err := tx.Model(db.Statement.Dest).Count(&pager.totalCount).Error; err != nil {
-		panic(err)
-	}
+	tx.Model(db.Statement.Dest).Count(&pager.totalCount)
 
 	tx.Statement.Preloads = preloads
 	tx.Statement.Clauses = clauses
